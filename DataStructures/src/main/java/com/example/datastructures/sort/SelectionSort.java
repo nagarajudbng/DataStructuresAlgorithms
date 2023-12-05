984363f1628410cb39b0e4e3d21a38dae22e28e2
nagarajudbng <nagaraj.dbng@gmail.com>
2023-12-05 23:25:04 +0530

    Selection Sort Algorithm added
    Selection Sort Algorithm added
    
    Co-Authored-By: nagarajudbng <nagaraj.dbng@gmail.com>

---
+++ DataStructures/src/main/java/com/example/datastructures/sort/SelectionSort.java
@@ -0,0 +1,32 @@
+package com.example.datastructures.sort;
+
+import java.util.Arrays;
+
+public class SelectionSort {
+    public static void main(String[] str){
+        int[] input = {60,20,40,30,50,10};
+        System.out.println("Input = "+ Arrays.toString(input));
+
+        int[] output= new SelectionSort().selectionSort(input);
+        System.out.println("Output = "+Arrays.toString(output));
+    }
+
+    private int[] selectionSort(int[] input) {
+        int n= input.length;
+        for(int i=0;i<n-1;i++){
+            int min = i;
+            for(int j=i+1;j<n;j++){
+                if(input[j]<input[min]){
+                    min = j;
+                }
+            }
+            if(min!=i) {
+                int x = input[i];
+                input[i] = input[min];
+                input[min] = x;
+            }
+        }
+        return input;
+    }
+
+}
