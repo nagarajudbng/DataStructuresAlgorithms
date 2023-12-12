b5c814e31c9fe29a214d83e5e0aafb29d2341d44
nagarajudbng <nagaraj.dbng@gmail.com>
2023-12-12 17:04:50 +0530

    Added Insertion sort
    Added Insertion sort

--- DataStructures/src/main/java/com/example/datastructures/sort/InsertionSort.java
+++ DataStructures/src/main/java/com/example/datastructures/sort/InsertionSort.java
@@ -5,6 +5,10 @@ import java.util.Arrays;
 public class InsertionSort {
     /*
     Insertion sort Iterates consuming one input element each repetition, and growing a sorted output list, at each iteration insertion sort removes one element form the input data finds the location it belongs within the sorted list and inserts it there, it repeats until no input element remain.
+
+     Best Case: The best case is when input is an array that is already sorted. In this case insertion sort has a linear running time (i.e., Θ(n)).
+
+    Worst Case: The simplest worst case input is an array sorted in reverse order.
      */
     public static void main(String[] str){
         int[] input = {60,20,40,30,50,10};
