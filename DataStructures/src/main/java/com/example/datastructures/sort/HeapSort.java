06c959723d07d405ac5552df53f392ea51b2accc
nagarajudbng <nagaraj.dbng@gmail.com>
2023-12-12 17:14:34 +0530

    Added Heap Sort algorithm Details
    Added Descritption

--- DataStructures/src/main/java/com/example/datastructures/sort/HeapSort.java
+++ DataStructures/src/main/java/com/example/datastructures/sort/HeapSort.java
@@ -2,6 +2,19 @@ package com.example.datastructures.sort;
 
 import java.util.Arrays;
 
+//Heap sort is a comparison-based sorting technique based on Binary Heap data structure. It is similar to the selection sort where we first find the minimum element and place the minimum element at the beginning. Repeat the same process for the remaining elements.
+
+/*
+First convert the array into heap data structure using heapify, then one by one delete the root node of the Max-heap and replace it with the last node in the heap and then heapify the root of the heap. Repeat this process until size of heap is greater than 1.
+
+Build a heap from the given input array.
+Repeat the following steps until the heap contains only one element:
+-Swap the root element of the heap (which is the largest element) with the last element of the heap.
+-Remove the last element of the heap (which is now in the correct position).
+-Heapify the remaining elements of the heap.
+The sorted array is obtained by reversing the order of the elements in the input array.
+ */
+
 public class HeapSort {
 
     public static void main(String[] arg){
